6b06db5730c4db808adaf845107555fa5c9ff82c
Tomas Novosad <tomas.novosad@swisscom.com>
2020-12-15 15:49:42 +0100

    test comment added

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -24,6 +24,7 @@ export class HeroDetailComponent implements OnInit {
   }
 
   getHero(): void {
+    // get hero info
     const id = +this.route.snapshot.paramMap.get('id');
     this.heroService.getHero(id).subscribe(hero => this.hero = hero);
   }
